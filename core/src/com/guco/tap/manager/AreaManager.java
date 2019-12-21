89b4dfbf1b6eda5b2d832b0f11d0ba7cfd8ba87e
Skronak <narvak.skronak@gmail.com>
2019-12-21 01:08:13 +0100

    Area - work in progress

---
+++ core/src/com/guco/tap/manager/AreaManager.java
@@ -0,0 +1,22 @@
+package com.guco.tap.manager;
+
+import com.guco.tap.game.TapDungeonGame;
+import com.guco.tap.screen.AreaScreen;
+
+public class AreaManager {
+
+    private AreaScreen areaScreen;
+    private TapDungeonGame game;
+
+    public AreaManager(TapDungeonGame game) {
+//        this.areaScreen = game.playScreen;
+    }
+
+    public void update() {
+
+    }
+
+    public void switchArea(){
+
+    }
+}

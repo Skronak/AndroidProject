a1a40aa44d36c07cc3973b1055193cca5ab72a45
Skronak <narvak.skronak@gmail.com>
2020-03-15 00:27:38 +0100

    Reprise du projet

--- core/src/com/guco/tap/menu/inventory/CharacterMenu.java
+++ core/src/com/guco/tap/menu/inventory/CharacterMenu.java
@@ -32,7 +32,7 @@ public class CharacterMenu extends AbstractMenu {
     private Label criticalRateLabel;
     private InventorySlotImage weaponSlot, bodySlot, headSlot, legSlot, armSlot, mantleSlot, ringSlot1, ringSlot2; // A RENOMER SELON LE TYPE DE SLOT
     private Label hpLabel;
-    private int SLOT_SIZE=40;
+    private int SLOT_SIZE = 40;
     private GameInformation gameInformation;
     private SpriterDto spriterDto;
     private TapDungeonGame game;
@@ -51,13 +51,13 @@ public class CharacterMenu extends AbstractMenu {
         customizeMenuTable();
         spriterDto = gameManager.loadDrawer(game.sb, "/animation.scml");
         spriterPlayer = new SpriterPlayer(spriterDto.data.getEntity(0));
-        spriterPlayer.setPosition(Constants.V_WIDTH/2, 220);
+        spriterPlayer.setPosition(Constants.V_WIDTH / 2, 220);
         spriterPlayer.setScale(0.5f);
         spriterPlayer.speed = 15;
         spriterPlayer.setAnimation("idle_sleeping");
 //        spriterPlayer.addListener(new PlayerListenerImpl(spriterPlayer, playScreen));
         spriterPlayer.characterMaps[1] = spriterPlayer.getEntity().getCharacterMap("sleepingHead");
-          //spriterPlayer.characterMaps[headMap] = spriterPlayer.getEntity().getCharacterMap(assetsManager.helmList.get(gameInformation.equipedHead).mapName);
+        //spriterPlayer.characterMaps[headMap] = spriterPlayer.getEntity().getCharacterMap(assetsManager.helmList.get(gameInformation.equipedHead).mapName);
 //        spriterPlayer.characterMaps[bodyMap] = spriterPlayer.getEntity().getCharacterMap(assetsManager.bodyList.get(gameInformation.equipedBody).mapName);
 
 
@@ -71,29 +71,30 @@ public class CharacterMenu extends AbstractMenu {
         parentTable.addActor(nameLabel);
 
         float padRight = parentTable.getWidth() - SLOT_SIZE - 15;
-        weaponSlot = addSlot(padLeft,300, gameInformation.equipedWeapon);
-        bodySlot = addSlot(padLeft,240, null);
-        headSlot = addSlot(padLeft,180,null);
-        legSlot = addSlot(padRight,300,null);
-        armSlot = addSlot(padRight,240,null);
-        mantleSlot = addSlot(padRight,180, null);
+        weaponSlot = addSlot(padLeft, 300, gameInformation.equipedWeapon);
+        bodySlot = addSlot(padLeft, 240, null);
+        headSlot = addSlot(padLeft, 180, null);
+        legSlot = addSlot(padRight, 300, null);
+        armSlot = addSlot(padRight, 240, null);
+        mantleSlot = addSlot(padRight, 180, null);
 
-        weaponSlot.addListener(new ClickListener(){
+        weaponSlot.addListener(new ClickListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 if (availableItemsPane.isVisible()) {
                     availableItemsPane.setVisible(false);
-                } else{
+                } else {
                     availableItemsPane.setVisible(true);
                 }
                 return true;
-            }});
+            }
+        });
     }
 
     private InventorySlotImage addSlot(float posX, float posY, Item item) {
         Texture slotTexture;
 
         if (item != null) {
-            slotTexture = new Texture(Gdx.files.internal("sprites/icon/item_frame_"+item.grade+".png"));
+            slotTexture = new Texture(Gdx.files.internal("sprites/icon/item_frame_" + item.grade + ".png"));
         } else {
             slotTexture = new Texture(Gdx.files.internal("sprites/icon/item_frame_0.png"));
         }
@@ -113,13 +114,9 @@ public class CharacterMenu extends AbstractMenu {
         weaponAttackLabel = new Label("atk: ", skin);
         table.add(weaponAttackLabel).colspan(4).left();
         TextButton equipButton = new TextButton("EQUIP", skin);
-        equipButton.addListener(new ClickListener(){
+        equipButton.addListener(new ClickListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                if (currentEquipedInventorySlot!=null) {
-                    currentEquipedInventorySlot.equipSlot(false);
-                }
-                currentSelectedInventorySlot.equipSlot(true);
-                currentEquipedInventorySlot = currentSelectedInventorySlot;
+                equipItem();
                 return true;
             }
         });
@@ -135,7 +132,7 @@ public class CharacterMenu extends AbstractMenu {
 
         List<Item> availableWeapon = gameInformation.weaponItemList;
         for (int i = 0; i < 5; i++) {
-            for (int y=0; y<5; y++) {
+            for (int y = 0; y < 5; y++) {
                 Texture slotTexture = new Texture(Gdx.files.internal("sprites/icon/off_icon.png"));
                 Texture selectTexture = new Texture(Gdx.files.internal("sprites/icon/select.png"));
                 Texture equipTexture = new Texture(Gdx.files.internal("sprites/icon/iconSelected.png"));
@@ -151,14 +148,13 @@ public class CharacterMenu extends AbstractMenu {
                 if (item != null) {
                     inventorySlotImage.addListener(new ClickListener() {
                         public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                            selectItem(currentSelectedInventorySlot);
-                            currentSelectedInventorySlot = inventorySlotImage;
+                            selectItem(inventorySlotImage);
                             return true;
                         }
                     });
                 }
                 currentInventorySlotImages.add(inventorySlotImage);
-                table.add(inventorySlotImage).size(50,50).pad(2);
+                table.add(inventorySlotImage).size(50, 50).pad(2);
                 if (currentEquipedItem == item) {
                     inventorySlotImage.equipSlot(true);
                 }
@@ -166,53 +162,59 @@ public class CharacterMenu extends AbstractMenu {
             table.row();
         }
 
-       parentTable.addActor(availableItemsPane);
-       availableItemsPane.setSize(parentTable.getWidth(),195);
-       availableItemsPane.setPosition(0,Constants.MENU_PAD_EXTERNAL_HEIGHT + 100 - Constants.MENU_PAD_INTERNAL - availableItemsPane.getHeight());
-       availableItemsPane.setVisible(false);
+        parentTable.addActor(availableItemsPane);
+        availableItemsPane.setSize(parentTable.getWidth(), 195);
+        availableItemsPane.setPosition(0, Constants.MENU_PAD_EXTERNAL_HEIGHT + 100 - Constants.MENU_PAD_INTERNAL - availableItemsPane.getHeight());
+        availableItemsPane.setVisible(false);
+    }
+
+    private void equipItem() {
+        if (currentEquipedInventorySlot != null) {
+            currentEquipedInventorySlot.equipSlot(false);
+        }
+        currentSelectedInventorySlot.equipSlot(true);
+        currentEquipedInventorySlot = currentSelectedInventorySlot;
     }
 
     private void selectItem(InventorySlotImage inventorySlotImage) {
-//        if (currentSelectedInventorySlot!=null) {
-//            currentEquipedInventorySlot.selectSlot(false);
-//        }
-//        for (int i = 0; i < currentInventorySlotImages.size(); i++) {
-//            currentInventorySlotImages.get(i).selectSlot(false);
-//        }
-
+        inventorySlotImage.selectSlot(true);
         if (currentSelectedInventorySlot != null) {
             currentSelectedInventorySlot.selectSlot(false);
         }
-        if (currentEquipedInventorySlot != null) {
-            currentEquipedInventorySlot.selectSlot(false);
-        }
+        currentSelectedInventorySlot = inventorySlotImage;
 
-        // TODO je reset le currentInventorySlotImage a false donc jamais appelé
-        if (currentSelectedInventorySlot.isSelected==true) {
-            currentSelectedInventorySlot.equipSlot(true);
-            currentEquipedInventorySlot = inventorySlotImage;
-        } else {
-            currentInventorySlotImages.get(inventorySlotImage.item.id).selectSlot(true);
-            spriterPlayer.characterMaps[0] = spriterPlayer.getEntity().getCharacterMap(gameInformation.weaponItemList.get(inventorySlotImage.item.id).mapName);
-            weaponAttackLabel.setText("Damage: " + inventorySlotImage.item.damageValue);
-        }
+        //if (currentSelectedInventorySlot.isSelected == true) {
+        //    currentSelectedInventorySlot.equipSlot(true);
+        //    currentEquipedInventorySlot = inventorySlotImage;
+        //} else {
+        //    currentInventorySlotImages.get(inventorySlotImage.item.id).selectSlot(true);
+        //    spriterPlayer.characterMaps[0] = spriterPlayer.getEntity().getCharacterMap(gameInformation.weaponItemList.get(inventorySlotImage.item.id).mapName);
+        //    weaponAttackLabel.setText("Damage: " + inventorySlotImage.item.damageValue);
+        //}
+
     }
 
     private void customizeMenuTable() {
-        characterNameLabel = new Label("",skin);
+        characterNameLabel = new Label("", skin);
         characterNameLabel.setFontScale(0.7f);
-        weaponDamageLabel = new Label("",skin);
+        weaponDamageLabel = new Label("", skin);
         weaponDamageLabel.setFontScale(0.7f);
-        criticalRateLabel = new Label("",skin);
+        criticalRateLabel = new Label("", skin);
         criticalRateLabel.setFontScale(0.7f);
-        hpLabel = new Label("99 G",skin);
+        hpLabel = new Label("99 G", skin);
         hpLabel.setFontScale(0.7f);
 
-        addMenuHeader("INVENTORY",2);
+        addMenuHeader("INVENTORY", 2);
     }
 
     @Override
     public void show() {
+        currentEquipedItem = gameInformation.equipedWeapon;
+
+        if (currentSelectedInventorySlot != null) {
+            currentSelectedInventorySlot.selectSlot(false);
+        }
+
         parentTable.setVisible(true);
     }
 
